7b6e5a4e8037812f24e5d089efdf90d5390f339f
duraipixel <durairaj.pixel@gmail.com>
2023-04-06 15:14:25 +0530

    image upload hide

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', [App\Http\Controllers\TestController::class, 'index']);
 Route::get('/test-mail', [App\Http\Controllers\TestController::class, 'sendMail']);
 Route::get('/test-invoice', [App\Http\Controllers\TestController::class, 'invoiceSample']);
-Route::get('/upload-image', [App\Http\Controllers\ImageUploadController::class, 'index']);
+// Route::get('/upload-image', [App\Http\Controllers\ImageUploadController::class, 'index']);
 
 Auth::routes();
 
