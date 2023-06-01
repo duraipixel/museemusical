729b0f4175015cc5990c57fabe04045d991b10e6
duraipixel <durairaj.pixel@gmail.com>
2023-06-01 16:40:02 +0530

    added

--- database/migrations/2023_06_01_110246_add_flat_charge_to_global_settings.php
+++ database/migrations/2023_06_01_110246_add_flat_charge_to_global_settings.php
@@ -6,11 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class AddFlatChargeToGlobalSettings extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::table('global_settings', function (Blueprint $table) {
