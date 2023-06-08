fe350e4ca4c99b5c308b0ebe568341c386965c1b
duraipixel <durairaj.pixel@gmail.com>
2023-06-08 11:52:51 +0530

    testing in teste

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -593,7 +593,7 @@ class CheckoutController extends Controller
                 $items_ins['discount_price'] = percentageAmountOnly($item['sale_prices']['strike_rate_original'], $item['sale_prices']['overall_discount_percentage']);
                 $items_ins['discount_percentage'] = $item['sale_prices']['overall_discount_percentage'] ?? 0;
                 $items_ins['tax_amount'] = $item['tax']['gstAmount'] ?? 0;
-                $items_ins['tax_percentage'] = $item['tax_percentage'] ?? 0;
+                $items_ins['tax_percentage'] = $cart_total['tax_percentage'] ?? $item['tax_percentage'] ?? 0;
                 $items_ins['sub_total'] = $item['sub_total'];
 
                 OrderProduct::create($items_ins);
