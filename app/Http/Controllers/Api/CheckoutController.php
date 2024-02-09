129c5910bfe68f88d48caa35258b3a7d10ac3b0b
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-02-09 15:28:12 +0530

    Update CheckoutController.php

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -229,7 +229,7 @@ class CheckoutController extends Controller
 
             return $data;
         } catch (Exception $e) {
-            dd($e->getMessage());
+           dd($e);
             return response()->json([
                 'status' => false,
                 'message' => $e->getMessage()
